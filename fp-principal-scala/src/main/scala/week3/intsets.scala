210f52faa0a66640af14673abfa3bd0638d6a489
yennanliu <f339339@gmail.com>
2020-06-20 11:01:02 +0800

    add week3/intsets.scala

---
+++ fp-principal-scala/src/main/scala/week3/intsets.scala
@@ -0,0 +1,41 @@
+// Scala Tree data structure demo
+
+package week3
+
+object intsets{
+
+// abstract classes can contain members which 
+// are missing an implementation 
+// (in our case, incl and contains)
+abstract class IntSet {
+    def incl(x : Int): IntSet
+    def contains(x : Int): Boolean
+}
+
+class Empty extends IntSet{
+    def contains(x : Int): Boolean = false
+    def incl(x : Int): IntSet = new NonEmpty(x, new Empty, new Empty)
+    override def toString = "."
+}
+
+class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet{
+    def contains(x: Int): Boolean = 
+        if (x < elem) left contains x 
+        else if (x > elem) right contains x
+        else true
+
+    def incl(x : Int): IntSet = 
+        if (x < elem) new NonEmpty(elem, left incl x, right)
+        else if (x > elem) new NonEmpty(elem, left, right incl x)
+        else this
+}
+
+ def main(args: Array[String]){
+
+    val t1 = new NonEmpty(3, new Empty, new Empty)
+    println(t1)
+    val t2 = t1 incl 4
+    println(t2)
+
+ }
+}
