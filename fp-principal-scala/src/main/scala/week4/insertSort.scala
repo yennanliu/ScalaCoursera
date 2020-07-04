df6bd605d25b42568ca13f7d374c57e704dfaeaa
yennanliu <f339339@gmail.com>
2020-07-05 00:52:20 +0800

    comment error code (main/scala/week4/insertSort.scala

--- fp-principal-scala/src/main/scala/week4/insertSort.scala
+++ fp-principal-scala/src/main/scala/week4/insertSort.scala
@@ -1,12 +1,22 @@
 
-package week4
+// package week4
 
-object insertSort{
-    def insert(x: Int, xs: List[Int]): List[Int] = xs match {
-      case List => List(x)
-      case y :: ys => if (x <= y) x :: xs else y :: insert(x, ys)
-    }
+// object insertSort{
+//   // https://stackoverflow.com/questions/10406064/insertion-sort-implementation-in-scala
 
-   def main(args: Array[String]){
-   }
-}
+//   def isort(xs: List[Int]): List[Int] =
+//     if (xs.isEmpty) Null
+//     else insert(xs.head, isort(xs.tail))
+
+//   def insert(x: Int, xs: List[Int]): List[Int] =
+//     if (xs.isEmpty || x <= xs.head) x :: xs
+//     else xs.head :: insert(x, xs.tail)
+
+// def insert(x: Int, xs: List[Int]): List[Int] = xs match {
+//       case List => List(x)
+//       case y :: ys => if (x <= y) x :: xs else y :: insert(x, ys)
+//     }
+
+//    def main(args: Array[String]){
+//    }
+// }
