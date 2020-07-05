5de20735052c9e92635892d8217eca0745b74cc6
yennanliu <f339339@gmail.com>
2020-07-05 09:12:31 +0800

    add main/scala/week5/ListLast.scala

---
+++ fp-principal-scala/src/main/scala/week5/ListLast.scala
@@ -0,0 +1,16 @@
+
+package wee5
+
+object ListLast{
+    def last[T](xs: List[T]): T = xs match {
+        case List() => throw new Error("last of empty list")
+        case List(x) => x 
+        case y :: ys => last(ys)
+    }
+
+ def main(args: Array[String]){
+    val a = Array(1,2,3,4)
+    println(a)
+
+    }
+}
