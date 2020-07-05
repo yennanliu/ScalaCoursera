da46fe4eaa33ce74eb310b58fbf850ca0f971624
yennanliu <f339339@gmail.com>
2020-07-05 09:36:04 +0800

    add /week5/ListReverse.scala

---
+++ fp-principal-scala/src/main/scala/week5/ListReverse.scala
@@ -0,0 +1,19 @@
+
+package wee5
+
+object ListReverse{
+
+    def reverse[T](xs: List[T]): List[T] = xs match {
+        case List() => xs
+        case y :: ys => reverse(ys) ++ List(y)   
+    }
+
+ def main(args: Array[String]){
+    val a = List(1,2,3,4)
+    val b = List(5,6,7)
+    val c = List()
+    println(a)
+    println(reverse(a))
+    println(reverse(c))
+  }
+}
