dc62fbedd8f708435c508e6f4d33056d4d540552
yennanliu <f339339@gmail.com>
2020-07-14 11:29:24 +0800

    update code/comment (week6/ScalarProduct.scala

--- fp-principal-scala/src/main/scala/week6/ScalarProduct.scala
+++ fp-principal-scala/src/main/scala/week6/ScalarProduct.scala
@@ -2,18 +2,19 @@
 package week6
 
 object ScalarProduct{
-    // method 1 
+    // method 1 : regular zip, map op
     def ScalarProduct1(xs : Vector[Double], ys: Vector[Double]): Double = 
         (xs zip ys).map(xy => xy._1 * xy._2).sum
-    // method2
+    // method2 : pattern match
     def ScalarProduct2(xs: Vector[Double], ys: Vector[Double]): Double = 
         (xs zip ys).map{ case (x, y) => x * y }.sum
 
  def main(args: Array[String]){
-    val a = Vector(1.423,2.432,3.423,4.345)
-    val b = Vector(0.00,100.435,99.4,3.14)
+    val a = Vector(10.0, 2.0, 3.0, 30.0)
+    val b = Vector(0.00,100.0, -1.0, 20.0)
     println(a)
     println(b)
+    println(a zip b)
     println(ScalarProduct1(a, b))
     println(ScalarProduct2(a, b))
 
