ccec6e9be2ce979c37c33611935357b88a01def2
yennanliu <f339339@gmail.com>
2020-08-15 15:14:31 +0800

    add /week4/Simulation.scala

---
+++ fp-design-in-scala/src/main/scala/week4/Simulation.scala
@@ -0,0 +1,27 @@
+// https://www.coursera.org/learn/progfun2/lecture/62Xed/lecture-4-6-discrete-event-simulation-implementation-and-test
+
+package week4
+
+import week4.BankAccount
+
+abstract class Simulation{
+
+	type Action = () => Unit
+
+	case class Event(time: Int, action: Action)
+
+	private var curtime = 0
+	def currentTime: Int = curtime
+
+	private var agenda: List[Event] = List()
+
+	private def insert(ag: List[Event], item: Event): List[Event] = ag match {
+		case first :: rest if first.time <= item.time => first :: insert(rest, item)
+		case _ => item :: ag 
+	}
+
+	def afterDelay(delay: Int)(block: => Unit): Unit = {
+		val item = Event(currentTime + delay, () => block)
+		agenda = insert(agenda, item)
+	}
+}
