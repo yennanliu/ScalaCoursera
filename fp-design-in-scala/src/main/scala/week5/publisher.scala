1632997edd9cec2b076e6629462557f03690cc4e
yennanliu <f339339@gmail.com>
2020-08-29 16:04:19 +0800

    add fp-design-in-scala/src/main/scala/week5/

---
+++ fp-design-in-scala/src/main/scala/week5/publisher.scala
@@ -0,0 +1,22 @@
+// https://www.coursera.org/learn/progfun2/lecture/ZmLWH/lecture-5-1-imperative-event-handling-the-observer-pattern
+
+package week5
+
+trait Subcriber{
+	def handler(pub: publisher)
+}
+
+trait publisher{
+
+	private var subcribers: Set[Subcriber] = Set()
+
+	def subcribe(subcriber: Subcriber): Unit = 
+		subcribers += subcriber
+
+	def unsubcribe(subcriber: Subcriber): Unit = 
+		subcribers -= subcriber
+
+	def publish(): Unit = 
+		subcribers.foreach(_.handler(this))
+
+}
